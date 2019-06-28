22acdf4543fd41c0e08a24cceda31dbdb676aafc
Axel Rayer <axel.rayer@gmail.com>
2019-06-28 12:23:42 +0200

    worked on

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-  story:any 
-  display:any;
 
-  constructor() {}
+export class Tab1Page {
+  display: any[];
+  story: any;
+  test: string;
+  words: any[];
+  nbCharsMax: number;
 
   thread() {
-    this.display = new Array(Math.ceil(this.story.length / 270));
+    const nbCharsMax = 250;
+    this.display = new Array();
+    this.display.push("");
+
+    if (this.story) {
+      const words = this.story.split(" ");
+      let rowIndex = 0;
+
+      words.forEach((word: string) => {
+        let currentRow = this.display[rowIndex];
+        if ((currentRow + " " + word).length > nbCharsMax) {
+          rowIndex++;
+          this.display.push(word);
+        } else {
+          this.display[rowIndex] = currentRow + " " + word;
+        }
+      });
+    }
   }
 }
